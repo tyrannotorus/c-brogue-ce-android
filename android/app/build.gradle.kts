9b01df96a5e1ae5e450385602ce4cf0d96f7dca2
tyrannotorus <tyrannotorus@gmail.com>
2026-07-28 18:51:08 -0300

    updated version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "org.broguece.game"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1150120
-        versionName = "1.15.1.20"
+        versionCode = 1150121
+        versionName = "1.15.1.21"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
